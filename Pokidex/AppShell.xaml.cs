4d24359e47d49fe30055728ecebae5239996420d
Dartanner <alun.powell.uni@googlemail.com>
2024-08-14 20:52:15 +0100

    Add project files.

---
+++ Pokidex/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pokidex
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pokidex/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Pokidex
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
